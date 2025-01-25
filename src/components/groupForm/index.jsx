59ac7f062f9caad4807f84228a64916b0a6d053c
vikram2580 <singh.vikramsingh06@gmail.com>
2025-01-24 19:33:05 -0500

    amount dropdown and group size dropdown options update

--- src/components/groupForm/index.jsx
+++ src/components/groupForm/index.jsx
@@ -13,6 +13,18 @@ const GroupForm = ({ onSubmit, data, setData }) => {
       label: `${start + index} ${start + index === 1 ? "person" : "people"}`,
     }));
   };
+  const amountOptions = [
+    { value: 5, label: "5" },
+    { value: 10, label: "10" },
+    { value: 15, label: "15" },
+    { value: 20, label: "20" },
+    { value: 25, label: "25" },
+    { value: 30, label: "30" },
+    { value: 35, label: "35" },
+    { value: 40, label: "40" },
+    { value: 45, label: "45" },
+    { value: 50, label: "50" },
+  ];
   return (
     <div className={styles.container}>
       <Input
@@ -32,16 +44,19 @@ const GroupForm = ({ onSubmit, data, setData }) => {
         placeholder="Group Size"
         defaultValue={data.groupSize ? data.groupSize : undefined}
         className={styles.select}
-        options={generateOptions(1, 100)}
+        options={generateOptions(1, 10)}
         value={data.groupSize ? data.groupSize : undefined}
         onChange={(value) => setData({ ...data, groupSize: value })}
       />
-      <Input
+      <Select
         placeholder="Amount"
-        className={styles.input}
-        value={data.amount}
-        onChange={(e) => setData({ ...data, amount: e.target.value })}
+        defaultValue={data.amount ? data.amount : undefined}
+        className={styles.select}
+        options={amountOptions}
+        value={data.amount ? data.amount : undefined}
+        onChange={(value) => setData({ ...data, amount: value })}
       />
+
       <div className={styles.frequencyContainer}>
         <span className={styles.label}>Frequency</span>
         <Radio.Group
