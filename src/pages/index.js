0f34e845a2ebb2f48a0943bf9a48cefe6d38adfd
jonasgodfrey <godfreyjtech2020@gmail.com>
2025-03-15 12:08:56 -0400

    from godfrey

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 export { default as SignInPage } from "./signin";
-export { default as Home } from "./home";
+export { default as Home } from "./Home";
 export { default as Dashboard } from "./dashboard";
 export { default as CreateGroup } from "./createGroup";
 export { default as JoinGroup } from "./joinGroup";
 export { default as GroupDetails } from "./groupDetails";
+
